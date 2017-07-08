b933787b1192001d68552e55d35f5548aa9485ce
Sodiet <Sodiet@users.noreply.github.com>
2017-07-08 13:11:27 +0500

    Deleted

--- ProMultitouchExport.sketchplugin/Contents/Sketch/lib_MD5.framework/Versions/A/Headers/lib_MD5.h
+++
@@ -1,9 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for lib_MD5.
-FOUNDATION_EXPORT double lib_MD5VersionNumber;
-
-//! Project version string for lib_MD5.
-FOUNDATION_EXPORT const unsigned char lib_MD5VersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <lib_MD5/PublicHeader.h>
